c51aff3bef2326a1c531cb78691ef560cc2595d5
zaheersha <a.zaheersha@gmail.com>
2026-03-02 20:09:45 +0530

    scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+import { useEffect } from "react";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "./components/ui/toaster";
@@ -10,11 +12,23 @@ import Categories from "./pages/Categories";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
+/* ✅ Scroll To Top Component */
+function ScrollToTop() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
 function Router() {
   return (
     <div className="min-h-screen flex flex-col selection:bg-primary/20">
       <Navbar />
       <main className="flex-grow pt-24 pb-16">
+        <ScrollToTop />
         <Switch>
           <Route path="/" component={Home} />
           <Route path="/categories" component={Categories} />
@@ -31,8 +45,10 @@ function Router() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster />
-      <Router />
+      <WouterRouter hook={useHashLocation}>
+        <Toaster />
+        <Router />
+      </WouterRouter>
     </QueryClientProvider>
   );
 }
